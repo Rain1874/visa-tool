4e70c76588401cb672815577011a90265608b073
rain <zhangxy@troy.cn>
2025-01-13 18:09:19 +0800

    feat: 打包部署

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,17 @@
+/*
+ * @Author: zhangxy
+ * @email: zhangxy@troy.cn
+ * @Date: 2025-01-10 17:05:03
+ * @LastEditors: zhangxy
+ * @LastEditTime: 2025-01-13 18:08:17
+ */
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from 'react';
 
 const ErrorBoundary = lazy(() => import('../pages/errorBoundary'));
 const Home = lazy(() => import('../pages/home'));
 const Login = lazy(() => import('../pages/login'));
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const routes = [
   {
@@ -12,13 +20,7 @@ const routes = [
     auth: false,
   },
   {
-    path: '/',
-    element: <Home />,
-    auth: true,
-    children: [],
-  },
-  {
-    path: '/auth/home',
+    path: isDevelopment ? '/' : '/dist/',
     element: <Home />,
     auth: true,
     children: [],
